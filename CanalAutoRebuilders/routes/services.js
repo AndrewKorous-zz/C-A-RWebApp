873449b53445184e41329e512e7312cb3f9f5511
AndrewKorous <ajkorous@gmail.com>
2016-08-13 14:50:49 -0500

    First iteration of Services page done, needs styling to make carousels regular (kill me)

--- CanalAutoRebuilders/routes/services.js
+++ CanalAutoRebuilders/routes/services.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/services', { title: 'Canal Auto Rebuilders' });
+  res.render('pages/services/services', { title: 'Canal Auto Rebuilders' });
 });
 
 module.exports = router;

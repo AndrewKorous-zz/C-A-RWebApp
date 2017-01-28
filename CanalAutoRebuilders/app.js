0a83a08d2b080bbcd892ec7cbf9f0595a3518d27
Andrew Korous <ajkorous@gmail.com>
2017-01-28 17:27:47 -0600

    configure database runs on server start now

--- CanalAutoRebuilders/app.js
+++ CanalAutoRebuilders/app.js
@@ -6,9 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
 
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-var configureDatabase = require('./database/configureDatabase.js')
+var mongoUtil = require('./database/mongoUtil');
 
 var routes = require('./routes/index');
 var services = require('./routes/services');
@@ -19,76 +17,70 @@ var testimonials = require('./routes/testimonials');
 var app = express();
 
 //database setup
-mongoose.connect(configureDatabase.url, function(error){
-  if(error){
-    console.log(error);
-  } else{
-    require('./database/seeds/users.js');
-  }
-});
+mongoUtil.connectToServer(function(error){
+  require('./database/configureDatabase');
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  app.use(lessMiddleware(path.join(__dirname, 'less'), {
+    dest: path.join(__dirname, 'public'),
+    options: {
+      compiler: {
+        compress: true
+      }
+    },
+    preprocess: {
+      path: function(pathname, req) {
+        return pathname.replace('/css/', '/');
+      }
+    },
+  }));
 
-app.use(lessMiddleware(path.join(__dirname, 'less'), {
-  dest: path.join(__dirname, 'public'),
-  options: {
-    compiler: {
-      compress: true
-    }
-  },
-  preprocess: {
-    path: function(pathname, req) {
-      return pathname.replace('/css/', '/');
-    }
-  },
-}));
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/', routes);
+  app.use('/services', services);
+  app.use('/about', about);
+  app.use('/contact-us', contact);
+  app.use('/testimonials', testimonials);
 
-app.use('/', routes);
-app.use('/services', services);
-app.use('/about', about);
-app.use('/contact-us', contact);
-app.use('/testimonials', testimonials);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('pages/error', {
-      message: err.message,
-      error: err
-    });
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
   });
-}
 
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+  // error handlers
 
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('pages/error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  // app.use(function(err, req, res, next) {
+  //   res.status(err.status || 500);
+  //   res.render('error', {
+  //     message: err.message,
+  //     error: {}
+  //   });
+  // });
+});
 
 module.exports = app;
--- CanalAutoRebuilders/database/configureDatabase.js
+++ CanalAutoRebuilders/database/configureDatabase.js
@@ -1,39 +1,29 @@
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-var url = 'mongodb://localhost:27017/canalauto';
+var mongoUtil = require('./mongoUtil');
+var db = mongoUtil.getDb();
 var users = require('./seeds/users');
 
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
+var adminSeed = {
+  email:'ajkorous@gmail.com',
+  password:'zpqm01',
+  firstName:'Andrew',
+  lastName:'Korous'
+}
+
+db.collection('users').findOne({'email':adminSeed.email}, function(error, user){
+  if(error){
+    console.log(error);
+  };
+  if(user){
+    console.log(adminSeed.firstName+' '+adminSeed.lastName+' was already seeded');
   } else {
-    console.log('Connection established to', url);
-    var userCollection = db.collection('users');
-    var admin = {
-      email:'ajkorous@gmail.com',
-      password:'zpqm01',
-      firstName:'Andrew',
-      lastName:'Korous'
-    };
-    userCollection.findOne({'email':admin.email}, function(error, user){
+    db.collection('users').insert([admin], function(error, result){
       if(error){
         console.log(error);
-      };
-      if(user){
-        console.log(admin.firstName + ' ' + admin.lastName + ' was already seeded');
       } else {
-        userCollection.insert([admin], function (err, result) {
-          if(err){
-            console.log(err);
-          } else {
-            console.log(admin.firstName + admin.lastName + 'inserted into db');
-          }
-          db.close();
-        });
+        console.log(adminSeed.firstName+' '+adminSeed.lastName+' was inserted into the db');
       }
+      db.close();
     })
-
-
-
   }
+  db.close();
 });
